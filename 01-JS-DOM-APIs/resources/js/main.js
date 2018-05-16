e28b186a07ec3bfdb578c85d79506c086934769e
Lucia <puccinilucia@gmail.com>
2018-05-16 02:08:57 -0300

    Ex 1
    fade elements onload

--- 01-JS-DOM-APIs/resources/js/main.js
+++ 01-JS-DOM-APIs/resources/js/main.js
@@ -1,15 +1,12 @@
-
 function callEvents() {
   var slideSource = document.getElementById('slideSource');
   document.getElementById('handle').onclick = function () {
     slideSource.classList.toggle('fade');
   };
 };
-
 window.onload = function() {
   // i can do this:
   //element.classList.toggle('fade');
   // but for clarity
   callEvents();
-
   };
