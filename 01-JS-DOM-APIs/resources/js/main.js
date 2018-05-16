f0a2d8ff5c8f195031aec875006f61ae72edbb8f
Lucia <puccinilucia@gmail.com>
2018-05-16 15:52:03 -0300

    Ex. 3
    incomplete, missing server error

--- 01-JS-DOM-APIs/resources/js/main.js
+++ 01-JS-DOM-APIs/resources/js/main.js
@@ -1,19 +1,35 @@
-function callEvents() {
-  var slideSource = document.getElementById('slideSource');
-  document.getElementById('handle').onclick = function () {
+function callEvents()
+{
+  let slideSource = document.getElementById('slideSource');
+  document.getElementById('handle').onclick = function ()
+  {
     slideSource.classList.toggle('fade');
   };
-  // Alert btn
-
 };
-window.onload = function() {
+window.onload = function()
+{
   // i can do this:
   //element.classList.toggle('fade');
   // but for clarity
   callEvents();
-  };
+};
+document.getElementById('alert-btn').onclick =function()
+{
+alert('this is an alert! o_o');
+};
+//fetching data
+document.getElementById('joke').addEventListener
+  ('click', fGetText);
+   function fGetText()
+    {
+      fetch('http://api.icndb.com/jokes/random')
+      .then((res) => res.text())
+      .then ((data) =>
+        {
+          document.getElementById('slideSource').innerHTML = data;
+        })
+          .catch((err) => console.log(err))
+            {
 
-  
-  document.getElementById('alert-btn').onclick =function(){
-    alert('this is an alert! o_o');
-  };
+            };
+    };
